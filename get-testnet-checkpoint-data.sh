637ec7c49110dbe8e0c27962c038d012cb02db74
Daniel Kmak <neverkuzi@gmail.com>
2021-07-20 14:57:24 +0200

    Update checkpoint data.

--- get-testnet-checkpoint-data.sh
+++ get-testnet-checkpoint-data.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ! -f ./20210704-nervos-ckb-indexer-testnet-snapshot.7z ]; then
-	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/20210704-nervos-ckb-indexer-testnet-snapshot.7z -L
+if [ ! -f ./20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z ]; then
+	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -L
 fi
 
 if [ -f ./indexer-data ]; then
 	rm -r ./indexer-data
 fi
 
-7z x 20210704-nervos-ckb-indexer-testnet-snapshot.7z -oindexer-data -aoa
+7z x 20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -oindexer-data -aoa
