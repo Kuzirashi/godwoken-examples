9fdfb343009e4a693d4c0e9fe1e458a5760a4b47
Daniel Kmak <neverkuzi@gmail.com>
2021-07-30 22:01:25 +0200

    Update snapshot data.

--- get-testnet-snapshot-data.sh
+++ get-testnet-snapshot-data.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -f ./20210728-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z ]; then
-	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/20210728-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -L
+target="./indexer-data-path/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606"
+
+if [ ! -f ./20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z ]; then
+	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -L
 fi
 
-if [ -f ./indexer-data ]; then
-	rm -r ./indexer-data
+if [ -d $target ]; then
+	rm -r $target
 fi
 
-7z x 20210728-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -oindexer-data -aoa
+7z x 20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -oindexer-data-path/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606 -aoa
