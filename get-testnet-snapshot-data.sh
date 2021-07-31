8fbf05737060c23280d9301162e593b0430b9ed9
Daniel Kmak <neverkuzi@gmail.com>
2021-07-31 11:02:50 +0200

    Update testnet config.

--- get-testnet-snapshot-data.sh
+++ get-testnet-snapshot-data.sh
@@ -2,12 +2,12 @@
 
 target="./indexer-data-path/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606"
 
-if [ ! -f ./20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z ]; then
-	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -L
+if [ ! -f ./20210730-nervos-lumos-indexer-0.16.0-testnet-snapshot.7z ]; then
+	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/20210730-nervos-lumos-indexer-0.16.0-testnet-snapshot.7z -L
 fi
 
 if [ -d $target ]; then
 	rm -r $target
 fi
 
-7z x 20210718-nervos-ckb-indexer-0.2.0-testnet-snapshot.7z -oindexer-data-path/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606 -aoa
+7z x 20210730-nervos-lumos-indexer-0.16.0-testnet-snapshot.7z -oindexer-data-path/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606 -aoa
