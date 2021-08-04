32afbf37ca837f7b59ab18cec94080ed2bd0e0e9
Daniel Kmak <neverkuzi@gmail.com>
2021-08-04 16:36:04 +0200

    Fix to-eth-address

--- packages/tools/src/account/address.ts
+++ packages/tools/src/account/address.ts
@@ -1,7 +1,8 @@
 import { HexString, Script, utils } from "@ckb-lumos/base";
-import { Godwoken } from "@godwoken-examples/godwoken";
+import { Godwoker } from "@polyjuice-provider/base";
 import commander from "commander";
 import { deploymentConfig } from "../modules/deployment-config";
+import { getRollupTypeHash } from "../modules/deposit";
 import { ROLLUP_TYPE_HASH } from "../modules/godwoken-config";
 
 function ethEoaAddressToGodwokenShortAddress(ethAddress: HexString): HexString {
@@ -17,18 +18,6 @@ function ethEoaAddressToGodwokenShortAddress(ethAddress: HexString): HexString {
   const scriptHash = utils.computeScriptHash(layer2Lock);
   const shortAddress = scriptHash.slice(0, 42);
   return shortAddress;
-}
-
-async function godwokenShortAddressToEthEoaAddress(
-  godwoken: Godwoken,
-  godwokenAddress: HexString
-): Promise<HexString> {
-  const scriptHash = await godwoken.getScriptHashByShortAddress(
-    godwokenAddress
-  );
-  const script = await godwoken.getScript(scriptHash);
-  const ethAddress = "0x" + script.args.slice(66);
-  return ethAddress;
 }
 
 export const toShortAddress = async (program: commander.Command) => {
@@ -48,13 +37,18 @@ export const toEthAddress = async (program: commander.Command) => {
   const shortAddress = program.shortAddress;
   const godwokenURL = program.parent.godwokenRpc;
 
-  const godwoken = new Godwoken(godwokenURL);
+  const godwoker = new Godwoker(godwokenURL, {
+    godwoken: {
+      eth_account_lock: {
+        code_hash: deploymentConfig.eth_account_lock.code_hash,
+        hash_type: deploymentConfig.eth_account_lock.hash_type as "data" | "type"
+      },
+      rollup_type_hash: getRollupTypeHash()
+    }
+  });
 
   try {
-    const ethAddress = await godwokenShortAddressToEthEoaAddress(
-      godwoken,
-      shortAddress
-    );
+    const ethAddress = await godwoker.getEthAddressByAllTypeShortAddress(shortAddress);
     console.log("eth eoa address:", ethAddress);
     process.exit(0);
   } catch (err) {
