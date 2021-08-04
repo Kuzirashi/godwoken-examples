3abdf96748341a642352282b8fadc811bd0e6e39
Daniel Kmak <neverkuzi@gmail.com>
2021-08-04 16:53:26 +0200

    Use newer bootstrap file.

--- get-testnet-snapshot-data.sh
+++ get-testnet-snapshot-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 indexerPath="./indexer-data-path/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606"
-snapshot=20210730-nervos-lumos-indexer-0.16.0-testnet-snapshot.7z
+snapshot=20210801-nervos-lumos-indexer-0.16.0-testnet-snapshot.7z
 
 if [ ! -f ./$snapshot ]; then
 	curl -O https://s3.amazonaws.com/cdn.ckb.tools/snapshots/$snapshot -L
